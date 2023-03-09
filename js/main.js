14c3f36700986f61c3badc34028d06eead007f77
JimHaff <james.haff@generalassemb.ly>
2023-03-09 18:41:42 +0100

    Added some new work for deployment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 console.log("JS TEST MAIN BRANCH!")
-console.log("Im on main branch!")
+console.log("Im on main branch!");
+console.log("I added somme new feature")
+console.log('I added some work')
